9340f3035235dd812fcc3e3ad7d1d4e1b7bb8720
ccpoveda <ccpoveda.programador@gmail.com>
2022-07-03 17:03:44 -0500

    ejercicios Repit

---
+++ js/ejerciciosRepit.js
@@ -0,0 +1,40 @@
+// ! Instrucciones - 1
+/*
+** Encuentra la última letra de un string **
+
+La función findLastLetter() recibe como parámetro un string cualquiera, tu labor es tomar ese string y obtener el último caracter e imprimirlo usando console.log()
+
+Ejemplo: Si el string que recibe es "Hola, me llamo Erik", en el console.log deberás imprimir la letra k
+*/
+
+// ? let data = "Hola, me llamo Erik";
+// ? let texto = data[data.length - 1];
+// ? console.log(texto);
+
+
+// !Instrucciones - 2
+/*
+** Encuentra la última palabra de un string **
+
+La función findLastWord() recibe como parámetro un string cualquiera, tu labor es tomar ese string y obtener la última palabra e imprimirla usando console.log()
+
+Ejemplo: Si el string que recibe es "Hola, me llamo Erik", con console.log() deberás imprimir la palabra Erik
+*/
+
+// ? let data = "Hola, me llamo Erik";
+// ? let ultimaPalabra = data.split(' ');
+// ? console.log(ultimaPalabra.pop());
+
+
+// !Instructions - 3
+/*
+** Encuentra la segunda palabra de un string **
+
+La función findSecondWord() recibe como parámetro un string cualquiera, tu labor es tomar ese string, obtener la segunda palabra e imprimirla usando console.log()
+
+Ejemplo: Si el string que recibe es "Hola, me llamo Erik", con console.log() deberás imprimir la palabra me
+*/
+
+// ? let data = "Hola, me llamo Erik";
+// ? let ultimaPalabra = data.split(' ' , ',');
+// ? console.log(ultimaPalabra[1]);
